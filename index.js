c2abfb8401eaddbefe0c5a20ef879207bbe636a2
Ferenczi Sámuel <33907257+FerencziSamu@users.noreply.github.com>
2019-04-09 14:12:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const port=process.env.PORT || 5000
-var index = fs.readFileSync('web/index.html')
+var fs = require('fs');
+var index = fs.readFileSync('web/index.html');
 const server = http.createServer((req, res) => {
 
 res.statusCode = 200;
