cf000b378735493f756bdea4e46f69b8c336a29a
Ferenczi Sámuel <33907257+FerencziSamu@users.noreply.github.com>
2019-04-10 10:13:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const http = require('http');
 const port=process.env.PORT || 5000
+var fs = require('fs');
 const server = http.createServer((req, res) => {
-
 res.statusCode = 200;
-
 res.setHeader('Content-Type', 'text/html');
-
-res.sendFile('web/index.html');
-
+  fs.readFile('index.html', function(err, data){
+    if(err){
+      return console.log(err);
+    }
+  res.end(data);
+  });
 });
-
 server.listen(port,() => {
-
 console.log(`Server running at port `+port);
-
 });
