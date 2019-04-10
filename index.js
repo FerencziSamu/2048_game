0af89816762ab69026e5125300d864aaf7ed3187
samuf <farkas.david05@gmail.com>
2019-04-10 11:25:44 +0200

    herokuuuu

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express');
+var bodyParser = require('body-parser');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('view engine', 'html')
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/public/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+
+
+app.get('*', function(req, res){
+    res.render('index.html');
+});
+
+app.listen(app.get('port'), function() {
+});
